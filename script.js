524cd45461356571cf40042aba92dfb77f629c51
Ishaaaqh <mohammedishaandsp@gmail.com>
2019-08-24 17:59:19 +0530

    changed the request from http to https

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ let getMoviesByText = searchText => {
   $.ajax({
     type: "GET",
     dataType: "json",
-    url: `http://www.omdbapi.com/?apikey=9e27197f&t=${searchText}`,
+    url: `https://www.omdbapi.com/?apikey=9e27197f&t=${searchText}`,
     success: response => {
       console.log(response);
       if (response.Response == "True") {
@@ -79,7 +79,7 @@ let getMoviesById = searchText => {
   $.ajax({
     type: "GET",
     dataType: "json",
-    url: `http://www.omdbapi.com/?apikey=9e27197f&i=${searchText}`,
+    url: `https://www.omdbapi.com/?apikey=9e27197f&i=${searchText}`,
     success: response => {
       if (response.Response == "True") {
         $("#moviePoster").html(`<img src=${response.Poster} class="image"/>`);
